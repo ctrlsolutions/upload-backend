4283894be1cacfc079577757f8de694408a6eb8c
Az <arbusinos@up.edu.ph>
2025-03-13 00:21:34 +0800

    [feat:partial][az] adds dashboard route

--- UPLoad/routers.py
+++ UPLoad/routers.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 
-from user.viewsets import AuthViewSet, GoogleAuthViewSet
+from user.viewsets import AuthViewSet, GoogleAuthViewSet, DashboardViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', AuthViewSet, basename="user")
 router.register(r'user/google', GoogleAuthViewSet, basename="google")
+router.register(r'dashboard', DashboardViewSet, basename="dashboard")
 
 urlpatterns = router.urls
