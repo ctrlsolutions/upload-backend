f1bc35d951eb26ecf704b85777ce0bc0bd1e1625
Lanieza <lblopez@up.edu.ph>
2025-04-14 14:55:49 +0800

    [feat][lanie] reset to develop and made serializers and viewsets for report history

--- report/serializers.py
+++ report/serializers.py
@@ -1,7 +1,39 @@
-from rest_framework import serializers
+from django.db import models
 from .models import Report
+from .serializers import ReportHistorySerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets
 
-class ReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Report
-        fields = '__all__'
+class ReportViewSet(viewsets.ViewSet):
+    @action(detail=False, methods=['get'], url_path='history', permission_classes=[IsAuthenticated])
+    def report_history(self, request):
+        user = request.user
+        user_role = user.role.code if user.role else None
+
+        
+        if user_role == 'F': # Faculty
+            reports = Report.objects.filter(user_id=user)
+        
+        elif user_role == 'DP': # Department Chair
+            reports = Report.objects.filter(
+                models.Q(user_id=user) | models.Q(department_id=user.department_id)
+            )
+
+        elif user_role == 'CD': # College Dean
+            reports = Report.objects.filter(
+                models.Q(user_id=user) | models.Q(college_id=user.college_id)
+            )
+
+        elif user_role == 'C': # Chancellor
+            reports = Report.objects.all()
+
+
+        else:
+            # default to own reports only if role is missing or unrecognized
+            reports = Report.objects.filter(user_id=user)
+
+        reports = reports.select_related('user_id__college', 'user_id__department')
+        serializer = ReportHistorySerializer(reports, many=True)
+        return Response(serializer.data)
