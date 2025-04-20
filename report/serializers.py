a61fb071d70f19502fdbb00a386d4a593c8cfaa6
Randall Alquicer <randall.bposeats@gmail.com>
2025-04-20 12:36:55 +0800

    tweaks to fix publication

--- report/serializers.py
+++ report/serializers.py
@@ -15,7 +15,7 @@ class ReportSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Report
-        fields = ['title', 'created_on', 'supporting_document']
+        fields = ['title', 'supporting_document']
         read_only_fields = ['id', 'user_id', 'created_on']
 
     def create(self, validated_data):
@@ -50,10 +50,7 @@ class ResearchSerializer(ReportSerializer):
 class PublicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublicationReport
-        fields = [
-            'report',
-            'id',
-            'publication_title',
+        fields = ReportSerializer.Meta.fields + [
             'publication_type',
             'author_names',
             'publication_date',
@@ -65,33 +62,14 @@ class PublicationSerializer(serializers.ModelSerializer):
             'issue_number',
             'doi_or_url',
             'isbn_or_issn',
-            'number_of_citations',
-            'file'
+            'number_of_citations'
         ]
 
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        publication_report_instance = PublicationReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return publication_report_instance
     
 class PaperSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResearchReport
-        fields = [
-            'report',
-            'id',
+        fields =  ReportSerializer.Meta.fields + [
             'research_title',
             'presented_paper_title',
             'presentation_type',
@@ -101,33 +79,14 @@ class PaperSerializer(serializers.ModelSerializer):
             'venue',
             'conference_start_date',
             'conference_end_date',
-            'presentation_date',
-            'file'
+            'presentation_date'
         ]
 
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        paper_report_instance = ResearchReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return paper_report_instance
     
 class PatentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResearchReport
-        fields = [
-            'report',
-            'id',
+        fields =  ReportSerializer.Meta.fields + [
             'patent_title',
             'patent_type',
             'application_no',
@@ -137,33 +96,13 @@ class PatentSerializer(serializers.ModelSerializer):
             'patent_grant_date',
             'registration_number',
             'commerical_product_name',
-            'industry_utilization',
-            'file'
+            'industry_utilization'
         ]
 
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        patent_report_instance = ResearchReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return patent_report_instance
-    
 class OtherResearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResearchReport
-        fields = [
-            'report',
-            'id',
+        fields =  ReportSerializer.Meta.fields + [
             'output_title',
             'output_type',
             'public_event_type',
@@ -174,33 +113,13 @@ class OtherResearchSerializer(serializers.ModelSerializer):
             'event_start_date',
             'event_end_date',
             'output_firstshownorreleasedtopublic_date',
-            'industry_utilization',
-            'file'
+            'industry_utilization'
         ]
-
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        other_research_report_instance = ResearchReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return other_research_report_instance
     
 class TrainingSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResearchReport
-        fields = [
-            'report',
-            'id',
+        fields =  ReportSerializer.Meta.fields + [
             'activity_type',
             'course_or_service_title',
             'venue',
@@ -209,33 +128,13 @@ class TrainingSerializer(serializers.ModelSerializer):
             'schedule_special_notes',
             'hours_required_to_complete',
             'number_of_trainees_served',
-            'source_of_funding',
-            'file'
+            'source_of_funding'
         ]
 
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        training_report_instance = ResearchReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return training_report_instance
-    
 class ExtensionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResearchReport
-        fields = [
-            'report',
-            'id',
+        fields =  ReportSerializer.Meta.fields + [
             'title',
             'components',
             'scope',
@@ -243,33 +142,13 @@ class ExtensionSerializer(serializers.ModelSerializer):
             'end_date',
             'target_beneficiary_group',
             'tbg_served',
-            'source_of_funding',
-            'file'
+            'source_of_funding'
         ]
-
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        extension_report_instance = ResearchReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return extension_report_instance
     
 class PartnershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResearchReport
-        fields = [
-            'report',
-            'id',
+        fields =  ReportSerializer.Meta.fields + [
             'type_of_extension_activities_under_this_partnership',
             'extension_partnership_title',
             'up_scope_of_work',
@@ -277,45 +156,11 @@ class PartnershipSerializer(serializers.ModelSerializer):
             'stakeholder_category',
             'partnership_agreement_type',
             'partnership_agreement_effectivity_start_date',
-            'partnership_agreement_effectivity_end_date',
-            'file'
+            'partnership_agreement_effectivity_end_date'
         ]
-
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        partnership_report_instance = ResearchReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return partnership_report_instance
 
 class OthersSerializer(serializers.ModelSerializer):
     class Meta:
         model = OthersReport
-        fields = ['report', 'description', 'file']
+        fields =  ReportSerializer.Meta.fields + ['report', 'description', 'file']
 
-    def create(self, validated_data):
-        report_data = validated_data.pop('report')
-        user = self.context['request'].user if 'request' in self.context else None
-        report_instance = None
-
-        if user and user.is_authenticated:
-            report_instance = Report.objects.create(**report_data, user_id=user)
-        else:
-            print("WARNING: Creating report without user during unauthenticated request.")
-            report_instance = Report.objects.create(**report_data)
-
-        others_report_instance = OthersReport.objects.create(
-            report_id=report_instance,
-            **validated_data
-        )
-        return others_report_instance
