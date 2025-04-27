10091766c241ca8e91cc660764f466fbf6384a9b
Az <arbusinos@up.edu.ph>
2025-04-27 16:49:58 +0800

    [fix][az] adds back college department viewset

--- UPLoad/routers.py
+++ UPLoad/routers.py
@@ -1,17 +1,16 @@
 from rest_framework import routers
 
-from user.viewsets import AuthViewSet, GoogleAuthViewSet, ProfileViewSet
-from report.viewsets import FormViewSet,FieldsViewSet, ResponseViewSet, ResponseDocumentViewSet, ReportViewSet, FormTemplateViewSet
+from user.viewsets import AuthViewSet, GoogleAuthViewSet, ProfileViewSet, CollegeDepartmentViewSet
+from report.viewsets import ResponseViewSet, ResponseDocumentViewSet, ReportViewSet, FormTemplateViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', AuthViewSet, basename="user")
 router.register(r'user/google', GoogleAuthViewSet, basename="google")
 router.register(r'profile', ProfileViewSet, basename="profile")
+router.register(r'cd', CollegeDepartmentViewSet, basename='cd')
 router.register(r'report', ReportViewSet, basename='report')
-router.register(r'forms',FormViewSet, basename='forms')
 router.register(r'ft', FormTemplateViewSet, basename='form-template')
-router.register(r'fields',FieldsViewSet, basename='fields')
 router.register(r'responses',ResponseViewSet, basename='responses')
 router.register(r'documents',ResponseDocumentViewSet, basename='documents')
 
