3fb88735ea4a3980b7209b094ba77ce5776f268c
Az <arbusinos@up.edu.ph>
2025-05-17 04:49:33 +0800

    [refactor][az] updates custom exception handler

--- UPLoad/utils.py
+++ UPLoad/utils.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
 
@@ -6,9 +7,9 @@ def custom_exception_handler(exc, context):
 
     if response is not None:
         return Response({
-            "success": False,
-            "error": response.data.get("detail", "An error occurred"),
-            "message": None
+            "error": response.data.get("detail", "An unexpected error occurred"),
         }, status=response.status_code)
 
-    return response
+    return Response({
+        "error": "An unhandled exception occurred"
+    }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
