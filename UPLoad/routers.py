8e1e92e3881f87f9f46a486f76fe6b3164f03759
Az <arbusinos@up.edu.ph>
2025-06-04 15:49:32 +0800

    [refactor][az] adds new college router

--- UPLoad/routers.py
+++ UPLoad/routers.py
@@ -1,13 +1,15 @@
 from rest_framework import routers
 
-from user.viewsets import AuthViewSet, GoogleAuthViewSet, ProfileViewSet, CollegeDepartmentViewSet
+from user.viewsets import AuthViewSet, GoogleAuthViewSet, ProfileViewSet
 from report.viewsets import ResponseViewSet, ResponseDocumentViewSet, ReportViewSet, FormViewSet
+from university.viewsets import CollegeDepartmentViewSet, CollegeViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', AuthViewSet, basename="user")
 router.register(r'user/google', GoogleAuthViewSet, basename="google")
 router.register(r'profile', ProfileViewSet, basename="profile")
+router.register(r'college', CollegeViewSet, basename='college')
 router.register(r'cd', CollegeDepartmentViewSet, basename='cd')
 router.register(r'form', FormViewSet, basename='form')
 router.register(r'report', ReportViewSet, basename='report')
